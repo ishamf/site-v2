0d81609b373c2fae5e3b6ae8c1de9a61a90c77ba
Isham Faizal <ishamf@users.noreply.github.com>
2024-02-01 23:10:23 +0800

    Improve current UI components

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -9,7 +9,9 @@
   on:click
   class="w-12 transition-colors group
 bg-slate-100 enabled:hover:bg-slate-200 disabled:bg-slate-200
-dark:bg-slate-700 dark:enabled:hover:bg-slate-600 dark:disabled:bg-slate-500"
+dark:bg-slate-700 dark:enabled:hover:bg-slate-600 dark:disabled:bg-slate-500
+focus:outline-none focus:ring-2 focus:ring-slate-500
+"
 >
   <svg
     viewBox="0 0 24 24"
--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -9,9 +9,12 @@
   bind:value
   {disabled}
   {placeholder}
-  class="flex-1 px-4 py-2 block min-w-0
+  class="flex-1 px-4 py-2 block min-w-0 min-h-12
 		bg-slate-100 placeholder:text-slate-400 text-slate-800
 		disabled:bg-slate-200 disabled:text-slate-400
 		dark:bg-slate-700 dark:placeholder:text-slate-400 dark:text-slate-200
-		dark:disabled:bg-slate-500 dark:disabled:text-slate-400"
+		dark:disabled:bg-slate-500 dark:disabled:text-slate-400
+    
+    focus:outline-none focus:ring-2 focus:ring-slate-500
+    "
 />
